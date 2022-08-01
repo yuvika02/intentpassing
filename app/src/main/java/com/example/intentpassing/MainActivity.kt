ee80133f768be307d3e57404516952bdd5688b59
yuvika02 <trikhayuvika20@gmail.com>
2022-08-01 21:31:01 +0530

    IMPLICT INTENT
    IMPLICIT INTENT

--- app/src/main/java/com/example/intentpassing/MainActivity.kt
+++ app/src/main/java/com/example/intentpassing/MainActivity.kt
@@ -3,33 +3,47 @@ package com.example.intentpassing
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btndial: Button
+    lateinit var btnemail:Button
+    lateinit var btnshare: Button
+    lateinit var btnrate: Button
+    lateinit var btnsms: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+         btndial = findViewById(R.id.dial)
+        btnemail = findViewById(R.id.email)
+        btnshare = findViewById(R.id.share)
+        btnrate = findViewById(R.id.rate)
+        btnsms = findViewById(R.id.sms)
 
-        var dialintent = Intent(Intent.ACTION_MAIN)
-        intent.data = Uri.parse("tel:0123456789")
-        startActivity(intent)
 
 
-        var emailintent = Intent(Intent.ACTION_VIEW)
+btndial.setOnClickListener {
+            var dialintent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:9988422624")
+            startActivity(intent) }
+btnemail.setOnClickListener {
+        var emailintent = Intent(Intent.ACTION_SENDTO)
         intent.data = Uri.parse("email: trikhayuvika20@gmail.com")
-        startActivity(intent)
+        startActivity(intent) }
 
-        var smsintent = Intent(Intent.ACTION_VIEW)
+btnsms.setOnClickListener {
+        var smsintent = Intent(Intent.ACTION_SENDTO)
         intent.data = Uri.parse("sms: hello")
-        startActivity(intent)
-
+        startActivity(intent) }
+btnshare.setOnClickListener {
         var shareintent = Intent(Intent.ACTION_VIEW)
         intent.data = Uri.parse("share: www.google.com")
-        startActivity(intent)
+        startActivity(intent) }
 
-        var rateintent = Intent(Intent.ACTION_VIEW)
+btnrate.setOnClickListener {
+        var rateintent = Intent(Intent.ACTION_ALL_APPS)
         intent.data = Uri.parse("Rate: 5")
         startActivity(intent)
-    }
+    } }
 }
